c23e20281154679dd191bbd0f873659f3596d4c0
Ki Suk Song <ksong@peacecorps.gov>
2019-12-30 09:36:24 -0500

    New Year Checker, Prints file path, Added README.md

--- Main.py
+++ Main.py
@@ -1,30 +1,35 @@
-import os, datetime
+import os
+import datetime
 import csv
 
-nurse_name = None
 now = datetime.datetime.now()
-month = None
-dates = None
-relative_path = None
-file_name = None
+year = now.year
 
 print("Nurse Schedule Date Calender")
+
 nurse_name = input("What is your Name?:  ")
 print("Hi! {},".format(nurse_name))
+
 month = input("What Month are you entering?(In numbers):  ")
 dates = input("What are the dates (Please Enter it in comma separated values. ex.1,2,15,20,25:  ")
+
 relative_path = os.path.expanduser("~/Downloads")
 file_name = relative_path + "/{} Day Shift {}-{}-{}.csv".format(nurse_name, month, now.day, now.year)
 
 # Todo: check value
+
 # Loop over dates
 _collections = []
 dates_list = dates.split(",")
-for day in dates_list:
-    _collections.append(
-        {"Subject": "{} Day Shift".format(nurse_name),
-         "Start Date": "{}/{}/{}".format(month, day, now.year)}
-    )
+
+
+# Generate Calendar to Next Year when a user enters next year
+def is_month_january(_month, _year):
+    ret = 0
+    if int(_month) == 1:
+        ret = int(_year) + 1
+
+    return ret
 
 
 # Save it as csv file
@@ -41,6 +46,14 @@ def store_into_csv(collections):
         writer.writerows(collections)
 
 
+year = is_month_january(month, year)
+
+for day in dates_list:
+    _collections.append(
+        {"Subject": "{} Day Shift".format(nurse_name),
+         "Start Date": "{}/{}/{}".format(month, day, year)}
+    )
+
 # Add value
 store_into_csv(_collections)
-print("Done!")
+print("Done! Files is Stored in: \n{}".format(file_name))
---
+++ README.md
@@ -0,0 +1,7 @@
+# User Guide
+1. Press Command - spacebar to launch Spotlight and type "Terminal"
+2. If the file is stored into the Desktop then enter following command. `cd ~/Dekstop/Nurse_Scheduler`
+3. Type `ls` to view files
+4. If you can see `Main.py` then type `python Main.py` to run the program
+5. Open csv file and verify the dates
+6. Go to https://support.google.com/calendar/answer/37118?co=GENIE.Platform%3DDesktop&hl=en&oco=0 and import CSV file to Google Calendar
